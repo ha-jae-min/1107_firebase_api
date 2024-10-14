f715b1f1b9e4bc2c5e2c3a30bf0f5c166ff66437
zk202308A <zk202308A@gmail.com>
2024-10-14 12:02:01 +0900

    aaa

--- src/main/java/org/zerock/api1014/product/repository/search/ProductSearch.java
+++ src/main/java/org/zerock/api1014/product/repository/search/ProductSearch.java
@@ -1,4 +1,13 @@
 package org.zerock.api1014.product.repository.search;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.zerock.api1014.product.domain.Product;
+
 public interface ProductSearch {
+
+    Page<Product> list( Pageable pageable);
+
+    Page<Product> listByCno(Long cno, Pageable pageable);
+
 }
