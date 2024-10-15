09f9d0bc52d3c3f1b813c042e609bdbbb3288a50
zk202308A <zk202308A@gmail.com>
2024-10-15 11:10:19 +0900

    1015_1

--- src/main/java/org/zerock/api1014/security/config/CustomSecurityConfig.java
+++ src/main/java/org/zerock/api1014/security/config/CustomSecurityConfig.java
@@ -3,8 +3,10 @@ package org.zerock.api1014.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class CustomSecurityConfig {
@@ -14,4 +16,14 @@ public class CustomSecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(
+                (auth) -> auth.requestMatchers("/api/v1/product/list").permitAll()
+        );
+
+        http.csrf(config -> config.disable());
+
+        return http.build();
+    }
 }
